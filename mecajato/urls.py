53e318e2ae33c516d95e90496261a7ab08c1921e
Gustavo Vaz Fernandes <gustavo.vaz@ufvjm.edu.br>
2024-11-05 21:05:52 -0300

    correção do login e criar conta

--- mecajato/urls.py
+++ mecajato/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('clientes/', include('clientes.urls')),
     path('', include('login.urls')),
-    path('accounts/login/', include('login.urls')),
+    path('login/', include('login.urls')),
     path('servicos/', include('servicos.urls')),
 ]
--- servicos/urls.py
+++ servicos/urls.py
@@ -2,7 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-   path('', include('login.urls')),
    path('novo_servico/', views.novo_servico, name="novo_servico"),
    path('listar_servico/', views.listar_servico, name="listar_servico"),
    path('servico/<str:identificador>/', views.servico, name="servico"),
